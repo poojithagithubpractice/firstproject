d949dbbeb341ae73d5dea6418dcac6a9bd1e6bd0
letsmovetocloud <letsmovetocloud@gmail.com>
2022-07-28 21:24:55 +0530

    second modulescompleted_vinay

--- ec2.tf
+++ ec2.tf
@@ -35,3 +35,14 @@ module "vpc" {
   public_subnets      = ["10.0.101.0/24"]
 
 #new comment added 
+
+resource "aws_s3_bucket" "terraform-s3-bucket-15-06-1991" {
+  bucket = "terraform-s3-bucket-15-06-1991"
+  acl   = "private_block"
+}
+resource "aws_s3_bucket_public_access_block" "terraform-s3-bucket-15-06-1991" {
+  bucket = aws_s3_bucket.terraform-s3-bucket-15-06-1991.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
